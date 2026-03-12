fixes and switch to mixed precision

models/decoders/disparity.py
@@ -1,7 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from models.decoders.dynamic_unet import DecoderBlock
+# from models.decoders.dynamic_unet import DecoderBlock
+from models.decoders.unext import DecoderBlock, LayerNorm2d, ConvNeXtBlock
 
 class CorrelationVolume(nn.Module):
     def __init__(self, max_displacement: int):
@@ -49,15 +50,15 @@ class DisparityDecoder(nn.Module):
         # Equalize left features and cost volume before concatenation to prevent one from dominating the other
         self.squeeze_left = nn.Sequential(
             nn.Conv2d(self.all_n_decoder_channels[0], max_displacement, kernel_size=1, bias=False),
-            nn.BatchNorm2d(max_displacement),
-            nn.ReLU(inplace=True)
+            LayerNorm2d(max_displacement),
+            nn.GELU()
         )
         
         # Adapter reduced the [Left Features + Cost Volume] channels back to the original number of decoder channels expected by the rest of the decoder blocks
         self.cost_volume_adapter = nn.Sequential(
             nn.Conv2d(max_displacement + max_displacement, self.all_n_decoder_channels[0], kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(self.all_n_decoder_channels[0]),
-            nn.ReLU(inplace=True),
+            LayerNorm2d(self.all_n_decoder_channels[0]),
+            nn.GELU(),
         )
         
         self.blocks = nn.ModuleList()
@@ -72,12 +73,12 @@ class DisparityDecoder(nn.Module):
         current_stride = self.all_decoder_increases[-1]
         if current_stride > 1:
             upsample_layers = []
+            last_channels = self.all_n_decoder_channels[-1]
             while current_stride > 1:
                 upsample_layers.append(nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True))
-                last_channels = self.all_n_decoder_channels[-1]
-                upsample_layers.append(nn.Conv2d(last_channels, last_channels, kernel_size=3, padding=1))
-                upsample_layers.append(nn.ReLU(inplace=True))
+                upsample_layers.append(ConvNeXtBlock(last_channels))
                 current_stride //= 2
+                
             self.final_block = nn.Sequential(*upsample_layers)
         else:
             self.final_block = nn.Identity()

models/decoders/dynamic_unet.py
@@ -42,9 +42,9 @@ class DecoderBlock(nn.Module): # A single decoder block with upsampling and skip
         return x
     
 class Decoder(nn.Module): # Assembles decoder blocks based on encoder channel list
-    def __init__(self, all_n_encoder_channels, all_encoder_reductions, is_stereo=False, stop_increases_at=1, intercept_at=1, **kwargs):
+    def __init__(self, all_n_encoder_channels, all_encoder_reductions, is_disparity=False, stop_increases_at=1, intercept_at=1, **kwargs):
         super().__init__()
-        self.is_stereo = is_stereo
+        self.is_disparity = is_disparity
         self.stop_increases_at = stop_increases_at
         self.intercept_at = intercept_at
         
@@ -66,7 +66,7 @@ class Decoder(nn.Module): # Assembles decoder blocks based on encoder channel li
             # For stereo input, double input channels for the first block to accommodate right image features
             # because both left and right features are concatenated
             # For other blocks, input channels remain the same
-            n_input_multiplier = 2 if self.is_stereo and i == 0 else 1
+            n_input_multiplier = 2 if self.is_disparity and i == 0 else 1
             
             # Number of decoder channels for this block 
             # Mono [i==0: 320, i==1: 112, i==2: 40, i==3: 24]
@@ -74,7 +74,7 @@ class Decoder(nn.Module): # Assembles decoder blocks based on encoder channel li
             n_decoder_channels = self.all_n_decoder_channels[i] * n_input_multiplier
             
             # For stereo, always double skip connection channels because skip features come from both left and right images
-            n_skip_multiplier = 2 if self.is_stereo else 1
+            n_skip_multiplier = 2 if self.is_disparity else 1
             
             # Skip connection channels coming from the next layer of the reversed encoder (falsely named, but kept for consistency)
             # Mono [i==0: 112, i==1: 40, i==2: 24, i==3: 16]
@@ -115,7 +115,7 @@ class Decoder(nn.Module): # Assembles decoder blocks based on encoder channel li
     def forward(self, encoder_features, encoder_features_right=None):
         # Reverse encoder features to start from the deepest layer
         reversed_encoder_features = encoder_features[::-1]
-        if not self.is_stereo:
+        if not self.is_disparity:
             # Start with the deepest features
             x = reversed_encoder_features[0]
         else:
@@ -129,20 +129,20 @@ class Decoder(nn.Module): # Assembles decoder blocks based on encoder channel li
         for i, block in enumerate(self.blocks):
             # Skip features come from the next layer in the reversed encoder features
             skip_features = reversed_encoder_features[i + 1]
-            if self.is_stereo:
+            if self.is_disparity:
                 skip_features_right = reversed_encoder_features_right[i + 1]
                 # Stereo: concate left and right skip features along channel dimension
                 skip_features = torch.cat([skip_features, skip_features_right], dim=1)
             
             x = block(x, skip_features)
             
-            if self.is_stereo and self.all_decoder_increases[i+1] == self.intercept_at:
+            if self.is_disparity and self.all_decoder_increases[i+1] == self.intercept_at:
                 intercept_features = x # Intercept features at 1/stop_increases_at resolution for disparity head
             
         # Final upsampling to original resolution    
         x = self.final_block(x)
         
-        if self.is_stereo:
+        if self.is_disparity:
             return x, intercept_features
         
         return x

models/decoders/unext.py
@@ -1,42 +1,58 @@
 import torch
 import torch.nn as nn
 
+class ConvNeXtBlock(nn.Module):
+    def __init__(self, channels):
+        super().__init__()
+        self.gamma = nn.Parameter(1e-6 * torch.ones(channels, 1, 1))
+        self.block = nn.Sequential(
+            nn.Conv2d(channels, channels, kernel_size=7, padding=3, groups=channels, bias=False),
+            LayerNorm2d(channels),
+            nn.Conv2d(channels, channels * 4, kernel_size=1, bias=True),
+            nn.GELU(),
+            nn.Conv2d(channels * 4, channels, kernel_size=1, bias=True),
+        )
+        
+    def forward(self, x):
+        return x + self.gamma * self.block(x) # Residual connection with learnable scaling factor gamma
+    
 class DecoderBlock(nn.Module): # A single decoder block with upsampling and skip connections
     def __init__(self, n_input_channels, n_skip_channels, n_output_channels):
         super().__init__()
+        self.gamma = nn.Parameter(1e-6 * torch.ones(n_output_channels, 1, 1))
         
-        # deviating from U-Net paper using nn.ConvTranspose2d to avoid checkerboard artifacts
-        # bilinear for smooth slopes to descent on (makes optimization easier vs nearest which is steppy)
-        # align_corner to prevent pixel shift
-        self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True) 
+        self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
         
-        self.double_conv = nn.Sequential(
-            # n_input_channels + n_skip_channels: number of channels from previous layer
-            # out_channels: desired output channels after convolution
-            # kernel_size=3x3 and padding=1: Maintain spatial dimensions, 2x 3x3 convs gives same receptive field than 1x 5x5 , but with less parameters
-            # bias false: because of BatchNorm adding its own bias
-            nn.Conv2d(n_input_channels + n_skip_channels, n_output_channels, kernel_size=3, padding=1, bias=False), 
-            # prevent neural network being a drama queen (exploding/vanishing gradients)
-            nn.BatchNorm2d(n_output_channels), 
-            # Non-linearity
-            # inplace saves memory
-            nn.ReLU(inplace=True), 
-            # Conv again, because U-Net paper uses double convs in each block
-            # Behaves like giving the model more thinking steps
-            # Retain n_output_channels as first conv already reduced channels
-            nn.Conv2d(n_output_channels, n_output_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(n_output_channels),
-            nn.ReLU(inplace=True),
-        )
+        # Depthwise convolution doesn't mix channels, so we need to mix it before
+        self.mix_channels = nn.Conv2d(n_input_channels + n_skip_channels, n_output_channels, kernel_size=1, bias=False) 
+        
+        self.convnext_block = ConvNeXtBlock(n_output_channels)
         
     def forward(self, x, skip_features=None):
         x = self.up(x)
         
-        if skip_features is not None: # if skip connection exists
-            # Concat upsampled features with skip connection features along channel dimension
-            # Expects both tensors to have same spatial dimensions -> divisible by 2 accordingly
+        if skip_features is not None:
             x = torch.cat([x, skip_features], dim=1)
             
-        x = self.double_conv(x)
+        x = self.mix_channels(x)
+        x = self.convnext_block(x) 
         
+        return x
+    
+class LayerNorm2d(nn.Module):
+    """Official LayerNorm implementation from the ConvNeXt paper (Channels First).
+    Calculates the mean and variance strictly across the channel dimension, 
+    perfectly preserving the spatial structure and contrast of the image.
+    """
+    def __init__(self, normalized_shape, eps=1e-6):
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(normalized_shape))
+        self.bias = nn.Parameter(torch.zeros(normalized_shape))
+        self.eps = eps
+
+    def forward(self, x):
+        u = x.mean(1, keepdim=True)
+        s = (x - u).pow(2).mean(1, keepdim=True)
+        x = (x - u) / torch.sqrt(s + self.eps)
+        x = self.weight[:, None, None] * x + self.bias[:, None, None]
         return x

models/manager.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from models.decoders.unext import LayerNorm2d
+
 
 from typing import Dict
 
@@ -30,22 +32,25 @@ class AttachHead(nn.Module):
         self.decoder = decoder_class(encoder_channels, encoder_reductions, **kwargs)
         self.head = nn.Conv2d(self.decoder.all_n_decoder_channels[-1], n_classes, kernel_size=1)
         
-        self.is_stereo = kwargs.get('is_stereo', False)
-        if self.is_stereo:
+        self.is_disparity = kwargs.get('is_disparity', False)
+        if self.is_disparity:
             intercept_at = kwargs.get('intercept_at', 4)
             intercept_channels = kwargs.get('intercept_channels', 128)
             idx = self.decoder.all_decoder_increases.index(intercept_at)
             n_output_channels = self.decoder.all_n_decoder_channels[idx]
             
             self.intercept_head = nn.Sequential(
-                nn.Conv2d(n_output_channels, intercept_channels, kernel_size=3, padding=1),
-                nn.ReLU(inplace=True),
-                nn.Conv2d(intercept_channels, intercept_channels, kernel_size=1),
+                # Swap to depthwise convolution, nn.Conv2d(n_output_channels, intercept_channels, kernel_size=3, padding=1),
+                nn.Conv2d(n_output_channels, n_output_channels, kernel_size=7, padding=3, groups=n_output_channels, bias=False), # Spatial Mixing, If OOM change to kernel_size=5, padding=2
+                LayerNorm2d(n_output_channels),
+                nn.Conv2d(n_output_channels, intercept_channels * 4, kernel_size=1), # Inverted Bottleneck with width expansion 4
+                nn.GELU(),
+                nn.Conv2d(intercept_channels * 4, intercept_channels, kernel_size=1),
             )
             
     
     def forward(self, features, feature_right=None) -> torch.Tensor:
-        if not self.is_stereo:
+        if not self.is_disparity:
             x = self.decoder(features)
             x = self.head(x)
             

processors/trainer.py
@@ -252,13 +252,14 @@ class Trainer(BaseProcessor):
             **optimizer_config['params'])
         logger.info(f'Optimizer: {optimizer_config["name"]} with params {optimizer_config["params"]}')
         
-        # TODO: fix that scheduler works with OneCycleLR
         scheduler_config = self.config['training']['scheduler']
         SchedulerClass = load(scheduler_config['name'])
         self.scheduler = SchedulerClass(
             self.optimizer,
             **scheduler_config['params'])
         logger.info(f'Scheduler: {scheduler_config["name"]} with params {scheduler_config["params"]}')
+        
+        self.scaler = torch.cuda.amp.GradScaler()
         
         train_log_interval = max(1, int(len(self.dataloader_train) * self.config['logging']['log_interval']))
         self.metrics_tracker = MetricsTracker(
@@ -304,7 +305,7 @@ class Trainer(BaseProcessor):
             right_images = data['right_image'].to(self.device) if 'right_image' in data else None
             targets = {task: data[task].to(self.device) for task in self.tasks}
 
-            with torch.set_grad_enabled(True):
+            with torch.set_grad_enabled(True) and torch.autocast(device_type='cuda', dtype=torch.float16):
                 outputs = self.model(left_images, right_images)
                 
                 if self.kd_models:
@@ -320,8 +321,11 @@ class Trainer(BaseProcessor):
                 loss, raw_task_losses = self.unweighted_sum_loss(outputs, targets)
                         
                 self.optimizer.zero_grad()
-                loss.backward()
-                self.optimizer.step()
+                self.scaler.scale(loss).backward()
+                self.scaler.unscale_(self.optimizer)
+                torch.nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=1.0) # !!! THIS DOESNT WORK WITH DISPARITY RANGE IN PIXELS !!!
+                self.scaler.step(self.optimizer)
+                self.scaler.update()
                 self.scheduler.step()
                 
                 with torch.no_grad():