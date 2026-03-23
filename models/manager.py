disp, mixed precision fixes

models/manager.py
@@ -54,7 +54,7 @@ class AttachHead(nn.Module):
             x, intercept_features = self.decoder(features, feature_right)
             
             x = self.head(x)
-            x = F.sigmoid(x) # TODO: or ReLU or softplus or nothing? 
+            x = F.relu(x, inplace=True) # x = F.leaky_relu(x, negative_slope=0.01, inplace=True) #F.relu(x, inplace=True) # TODO: sigmoid or ReLU or softplus or nothing? # AFTER EATING, TRY SIGMOID                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
             intercept_features = self.intercept_head(intercept_features)
             
             return {

notebooks/tools/find_learning_rate.py
@@ -5,6 +5,7 @@ sys.path.append(os.path.dirname('/data/Zeitler/code/SIDE/'))
 
 import yaml
 
+import torch
 import torch.nn as nn
 from torch_lr_finder import LRFinder, TrainDataLoaderIter
 
@@ -17,10 +18,10 @@ os.chdir('/data/Zeitler/code/SIDE')
 setup_environment()
 
 # %% Settings
-EXPERIMENT = 'DISP'
-START_LR = 1e-7
-END_LR = 10
-NUM_ITER = 100
+EXPERIMENT = 'debug'
+START_LR = 1e-8
+END_LR = 1.0
+NUM_ITER = 200
 
 # %%
 with open('./configs/base.yaml', 'r') as f: base_config = yaml.safe_load(f)
@@ -35,6 +36,8 @@ class LossComposerWrapper(nn.Module):
         self.loss_composer = loss_composer
         
     def forward(self, outputs, targets):
+        targets = {k: v.float() if isinstance(v, torch.Tensor) else v for k, v in targets.items()}
+        
         # LossComposer returns: inter_loss, inter_loss_weights, intra_losses, intra_loss_weights, raw_task_losses
         inter_loss, _, _, _, _ = self.loss_composer(outputs, targets)
         return inter_loss
@@ -45,10 +48,15 @@ class ModelInputWrapper(nn.Module):
         self.model = model
         
     def forward(self, inputs):
-        if isinstance(inputs, dict):
-            return self.model(inputs['image'], inputs.get('right_image'))
+        with torch.amp.autocast('cuda', dtype=torch.float16):
+            if isinstance(inputs, dict):
+                return self.model(inputs['image'], inputs.get('right_image'))
+            else:
+                outputs = self.model(inputs)
         
-        return self.model(inputs)
+        outputs = {k: v.float() if isinstance(v, torch.Tensor) else v for k, v in outputs.items()}
+        
+        return outputs
 
 class DictToDevice:
     def __init__(self, data_dict):
@@ -90,6 +98,7 @@ train_iter = MultiTaskIter(trainer.dataloader_train)
 
 optimizer_config = config['training']['optimizer']
 optimizer_config['params']['lr'] = START_LR
+optimizer_config['params']['weight_decay'] = 0.0
 optimizer_class = load(optimizer_config['name'])
 optimizer = optimizer_class(
     model.parameters(),
@@ -104,7 +113,7 @@ lr_finder.range_test(
     train_loader=train_iter,
     end_lr=END_LR,
     num_iter=NUM_ITER)
-lr_finder.plot(suggest_lr=True)
+lr_finder.plot(suggest_lr=False)
 lr_finder.reset()
 
 # %%

processors/trainer.py
@@ -283,6 +283,8 @@ class Trainer(BaseProcessor):
             **scheduler_config['params'])
         logger.info(f'Scheduler: {scheduler_config["name"]} with params {scheduler_config["params"]}')
         
+        self.scaler = torch.amp.GradScaler(str(self.device).split(':')[0])
+        
         train_log_interval = max(1, int(len(self.dataloader_train) * self.config['logging']['log_interval']))
         self.metrics_tracker = MetricsTracker(
             tasks=self.tasks,
@@ -328,7 +330,8 @@ class Trainer(BaseProcessor):
             right_images = data['right_image'].to(self.device) if 'right_image' in data else None
             targets = {task: data[task].to(self.device) for task in self.tasks}
 
-            with torch.set_grad_enabled(True), torch.amp.autocast(str(self.device), dtype=torch.bfloat16):
+            self.optimizer.zero_grad()
+            with torch.autocast(device_type=str(self.device).split(':')[0], dtype=torch.float16):
                 outputs = self.model(left_images, right_images)
                 
                 if self.kd_models:
@@ -341,11 +344,17 @@ class Trainer(BaseProcessor):
                         outputs[f'{task}_distillation'] = outputs[task]
                         targets[f'{task}_distillation'] = teacher_outputs
                 
-                inter_loss, inter_loss_weights, intra_losses, intra_loss_weights, raw_task_losses = self.loss_composer(outputs, targets)
-                        
-            self.optimizer.zero_grad()
-            inter_loss.backward()
-            self.optimizer.step()
+            
+            outputs = {k: v.float() for k, v in outputs.items()}
+            targets = {k: v.float() for k, v in targets.items()}
+                
+            inter_loss, inter_loss_weights, intra_losses, intra_loss_weights, raw_task_losses = self.loss_composer(outputs, targets)
+            
+            self.scaler.scale(inter_loss).backward()
+            self.scaler.unscale_(self.optimizer)
+            torch.nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=1.0)
+            self.scaler.step(self.optimizer)
+            self.scaler.update()
             self.scheduler.step()
                                 
             self.metrics_tracker.update(inter_loss.item(), inter_loss_weights, intra_losses, intra_loss_weights, raw_task_losses)
@@ -372,7 +381,11 @@ class Trainer(BaseProcessor):
             targets = {task: data[task].to(self.device) for task in self.tasks}
             
             with torch.no_grad():
-                outputs = self.model(left_images, right_images)
+                with torch.autocast(device_type=str(self.device).split(':')[0], dtype=torch.float16):
+                    outputs = self.model(left_images, right_images)
+                
+                outputs = {k: v.float() for k, v in outputs.items()}
+                targets = {k: v.float() for k, v in targets.items()}
                 
                 inter_loss, inter_loss_weights, intra_losses, intra_loss_weights, raw_task_losses = self.loss_composer(outputs, targets)
                 total_loss_weighted += inter_loss.item()